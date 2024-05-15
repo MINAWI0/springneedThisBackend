3200c089c2345683abe89120e7d15d2a25e0ea55
MohamedMinaoui <minaouimh@gmail.com>
2024-05-15 15:45:44 +0200

    for updating

--- src/main/java/com/example/springneedthisbackend/config/AppConfig.java
+++ src/main/java/com/example/springneedthisbackend/config/AppConfig.java
@@ -24,7 +24,9 @@ public class AppConfig {
     public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception {
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
-                .authorizeHttpRequests(Authorize -> Authorize.requestMatchers("/api/**").authenticated()
+                .authorizeHttpRequests(Authorize -> Authorize
+                        .requestMatchers("/").permitAll()
+                        .requestMatchers("/api/**").authenticated()
                         .anyRequest().permitAll()
                 ).addFilterBefore(new JwtTokenValidator(), BasicAuthenticationFilter.class)
                 .csrf().disable()
