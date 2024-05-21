50006c5b7c0be6d19c583d1c433a13aa4528e5ae
MohamedMinaoui <minaouimh@gmail.com>
2024-05-21 20:06:59 +0200

    for sharing !!

--- src/main/java/com/example/springneedthisbackend/config/AppConfig.java
+++ src/main/java/com/example/springneedthisbackend/config/AppConfig.java
@@ -39,7 +39,7 @@ public class AppConfig {
             @Override
             public CorsConfiguration getCorsConfiguration (HttpServletRequest request) {
                 CorsConfiguration cfg = new CorsConfiguration();
-                cfg.setAllowedOrigins (Arrays.asList("http://localhost:3000"));
+                cfg.setAllowedOrigins (Arrays.asList("http://localhost:3000" , "https://needthisapp-minawi0s-projects.vercel.app"));
                 cfg.setAllowedMethods(Collections.singletonList("*"));
                 cfg.setAllowCredentials(true);
                 cfg.setAllowedHeaders(Collections.singletonList("*"));
